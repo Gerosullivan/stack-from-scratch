d181485067d62073bab1a218b19afca3a8a6bd6d
Ger O'Sullivan <gerosullivan@gmail.com>
2017-04-27 12:39:09 +0100

    WebPack server working

--- src/client/config.js
+++ src/client/config.js
@@ -1,9 +1,3 @@
-// @flow
-
-export const WEB_PORT = process.env.PORT || 8000
-export const STATIC_PATH = '/static'
-export const APP_NAME = "Ger's App"
-
 export const WDS_PORT = 7000
 
 export const APP_CONTAINER_CLASS = 'js-app'
